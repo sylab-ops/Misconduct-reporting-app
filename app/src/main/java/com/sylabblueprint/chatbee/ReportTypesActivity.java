f6cf597c6002d8d6922789397b476def5aa1cc2a
“Abraham <mrabblessedslyst4christ099@gmail.com>
2021-04-02 09:33:52 +0300

    registration & userFragment

---
+++ app/src/main/java/com/sylabblueprint/chatbee/ReportTypesActivity.java
@@ -0,0 +1,14 @@
+package com.sylabblueprint.chatbee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReportTypesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report_types);
+    }
+}
