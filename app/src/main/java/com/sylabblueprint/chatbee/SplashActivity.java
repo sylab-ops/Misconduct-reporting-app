7b5f24dc6ba80bf782de976c1da4c89888765198
Girish Nalawade <girishnalawadegsn@gmail.com>
2021-05-17 04:45:50 +0530

    add login link on splash screen

--- app/src/main/java/com/sylabblueprint/chatbee/SplashActivity.java
+++ app/src/main/java/com/sylabblueprint/chatbee/SplashActivity.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
 
     Button getstartedBtn;
+    TextView gotologin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +19,7 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
 
         getstartedBtn = findViewById(R.id.getstartedBtn);
+        gotologin = findViewById(R.id.gotologin);
 
         getstartedBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,6 +28,12 @@ public class SplashActivity extends AppCompatActivity {
             }
         });
 
+        gotologin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+            }
+        });
     }
 }
 
